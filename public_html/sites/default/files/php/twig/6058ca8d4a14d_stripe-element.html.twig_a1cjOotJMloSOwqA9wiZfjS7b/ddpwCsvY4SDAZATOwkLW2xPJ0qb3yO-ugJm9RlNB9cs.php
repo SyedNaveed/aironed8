704dbc5515396c5a1c661fe14c3e1c98922c1c4d
naveed <you@example.com>
2021-03-23 19:53:50 +0500

    user pages set

---
+++ public_html/sites/default/files/php/twig/6058ca8d4a14d_stripe-element.html.twig_a1cjOotJMloSOwqA9wiZfjS7b/ddpwCsvY4SDAZATOwkLW2xPJ0qb3yO-ugJm9RlNB9cs.php
@@ -0,0 +1,111 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/stripe/templates/stripe-element.html.twig */
+class __TwigTemplate_a69507be538320beb6d09503d5eaa43b3481984588266b0326cfb1fadc51f625 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 1];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<div id=\"";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["id"] ?? null)), "html", null, true);
+        echo "-card-element\" >
+  ";
+        // line 3
+        echo "</div>
+
+";
+        // line 6
+        echo "<div id=\"";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["id"] ?? null)), "html", null, true);
+        echo "-card-errors\" class=\"sripe-card-errors\" role=\"alert\"></div>
+
+";
+        // line 9
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null)), "html", null, true);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/stripe/templates/stripe-element.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  70 => 9,  64 => 6,  60 => 3,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<div id=\"{{ id }}-card-element\" >
+  {# a Stripe Element will be inserted here. #}
+</div>
+
+{#  Used to display form errors #}
+<div id=\"{{ id }}-card-errors\" class=\"sripe-card-errors\" role=\"alert\"></div>
+
+{#  Render the element itself #}
+{{ children }}
+", "modules/contrib/stripe/templates/stripe-element.html.twig", "/var/www/aironem/public_html/modules/contrib/stripe/templates/stripe-element.html.twig");
+    }
+}
