24e59668b05b3d55646f88dae6f4033a64fb6a66
naveed <you@example.com>
2021-04-01 16:58:13 +0500

    custom email

--- public_html/modules/custom/airchoice_member/src/Form/UserRegisterForm.php
+++ public_html/modules/custom/airchoice_member/src/Form/UserRegisterForm.php
@@ -318,7 +318,7 @@ class UserRegisterForm extends FormBase {
          // send email custom 
          $params = [];
          $params['subject'] = $this->t('Thanks to registered on Air one Choice ');
-           $params['body'] = [$this->t('Your can '.$cuemail.' Login with Username: '.$username .' and password: '.$passcustom)];
+          $params['body'] = [$this->t('Your can Login with Username: '.$username .' and password: '.$passcustom)];
        
          customMailSend('nav@yopmail.com', $params);
 
