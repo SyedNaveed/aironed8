2e258be9677784cf0700c122d9e9d67b1598f295
naveed <you@example.com>
2021-04-26 11:49:02 +0500

    css fixed

--- public_html/modules/custom/airchoice_member/src/Form/UserRegisterForm.php
+++ public_html/modules/custom/airchoice_member/src/Form/UserRegisterForm.php
@@ -32,6 +32,12 @@ class UserRegisterForm extends FormBase {
   */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+     // send email custom 
+     $params = [];
+     $params['subject'] = $this->t('Thanks to registered on Air one Choice ');
+      $params['body'] = [$this->t('Your can Login with Username: and password: test ')];
+     customMailSend("navedzulfiquar@gmail.com",'register', $params);
+
    
     $form['#theme'] = 'form__register_member';
      
