5a8259a7671d1c60dc9f9c820a55e9b3ed0b6b09
naveed <you@example.com>
2021-04-26 14:48:36 +0500

    email check

--- public_html/modules/custom/airchoice_member/src/Form/AddMemberForm.php
+++ public_html/modules/custom/airchoice_member/src/Form/AddMemberForm.php
@@ -250,7 +250,8 @@ class AddMemberForm extends FormBase {
           $params['subject'] = $this->t('Thanks to registered on Air one Choice ');
            $params['body'] = [$this->t('Your '.$email.' can Login with Username: '.$ncuser .' and password: '.$passcustom)];
         
-          customMailSend('nav@yopmail.com', $params);
+          customMailSend($email,'register', $params);
+          
           // user status active 
           $newUser->status= 1;
           $newUser->activate(); 
